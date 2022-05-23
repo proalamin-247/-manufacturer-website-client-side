3f2818fa25eff6a04cc932c5712011983e4d479e
proalamin-247 <pro.alamin247@gmail.com>
2022-05-23 18:21:46 +0600

    replace review fack data and fetch data in database url

--- src/Pages/Home/Review.js
+++ src/Pages/Home/Review.js
@@ -2,19 +2,20 @@ import React from 'react';
 
 const reviewCard = ({ review }) => {
     return (
-        <div className="card lg:mx-w-lg bg-base-100 shadow-xl">
+        <div className="card lg:mx-w-lg bg-base-100 shadow-xl ">
             <div className="card-body">
                 <p>{review.review}</p>
-                <div className="flex items-center mt-5">
+                <div className=" grid justify-items-center mt-5">
                     <div className="avatar">
-                        <div className=" rounded-full ring ring-primary ring-offset-base-100 mr-5">
+                        <div className="rounded-full ring ring-primary ring-offset-base-100 mr-5">
                             <img src={review.img} alt="" />
                         </div>
-                        <div>
-                            <h4 className='text-xl'>{review.name}</h4>
-                            <h4 className='text-xl'>{review.location}</h4>
-                        </div>
                     </div>
+                </div>
+                <div>
+                    <h4 className='text-2xl'>{review.name}</h4>
+                    <h4 className='text-xl'>{review.city}</h4>
+                    <h4 className='text-xl'>Ratting <span className='text-primary'>{review.ratting}</span> out of 10</h4>
                 </div>
             </div>
         </div>
--- src/Pages/Home/Reviews.js
+++ src/Pages/Home/Reviews.js
@@ -1,39 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Review from './Review'
 
 const Reviews = () => {
-    const reviews = [
-        {
-            _id: 1,
-            name: 'Winson Herry',
-            review: 'It is a long established fact that by the readable content of a lot layout. The point of using Lorem a more-or-less normal distribu to using Content here, content',
-            location: 'california',
-            img: 'https://i.ibb.co/cYBsDbb/people1.png'
-        },
-        {
-            _id: 2,
-            name: 'Winson Herry',
-            review: 'It is a long established fact that by the readable content of a lot layout. The point of using Lorem a more-or-less normal distribu to using Content here, content',
-            location: 'california',
-            img: 'https://i.ibb.co/W0KqSrM/people2.png'
-        },
-        {
-            _id: 3,
-            name: 'Winson Herry',
-            review: 'It is a long established fact that by the readable content of a lot layout. The point of using Lorem a more-or-less normal distribu to using Content here, content',
-            location: 'california',
-            img: 'https://i.ibb.co/WPpcFgY/people3.png'
-        },
-    ];
+    const [reviews, setReviews] = useState([]);
 
-
+    useEffect(() => {
+        fetch('http://localhost:5001/review')
+            .then(res => res.json())
+            .then(data => setReviews(data));
+    }, [])
 
     return (
         <div className='my-12 p-5'>
             <h3 className='text-3xl text-primary font-bold py-2'>Latest Reviews</h3>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
                 {
-                    reviews.map(review => <Review
+                    reviews.slice(0, 3).map(review => <Review
                         key={review._id}
                         review={review}
                     ></Review>)
