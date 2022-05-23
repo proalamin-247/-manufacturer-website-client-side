06249e420bc416c0095130f23747c12b8222d366
proalamin-247 <pro.alamin247@gmail.com>
2022-05-23 11:23:59 +0600

    footer added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import Footer from '../Sheard/Footer';
 import Banner from './Banner';
+import BusinessSummary from './BusinessSummary';
+import Reviews from './Reviews';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            {/* <BusinessSummary></BusinessSummary> */}
+            <Reviews></Reviews>
+            <Footer></Footer>
         </div>
     );
 };
---
+++ src/Pages/Home/Reviews.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Reviews = () => {
+    
+
+
+
+    return (
+        <div>
+            <h3 className='text-xl text-primary font-bold py-2'>Latest Reviews</h3>
+            <div>
+
+            </div>
+        </div>
+    );
+};
+
+export default Reviews;
