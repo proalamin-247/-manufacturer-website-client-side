bec94b343489526abcf5e31b58103dd407609d61
proalamin-247 <pro.alamin247@gmail.com>
2022-05-23 11:38:43 +0600

    loading element create and firbase, react firebase hooks, react from install

---
+++ src/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDePwMAw8UPuAGKfp-IrpMXVqn8b7RrzY0",
+    authDomain: "manufacturer-website-8e809.firebaseapp.com",
+    projectId: "manufacturer-website-8e809",
+    storageBucket: "manufacturer-website-8e809.appspot.com",
+    messagingSenderId: "254516502494",
+    appId: "1:254516502494:web:c636e7442ccdbf0c95074f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
