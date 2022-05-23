98e7a3fe761c2abef6011847ff49fcecb139b08a
proalamin-247 <pro.alamin247@gmail.com>
2022-05-23 11:30:28 +0600

    make fack data and show reviews home page

---
+++ src/Pages/Home/Review.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const reviewCard = ({ review }) => {
+    return (
+        <div className="card lg:mx-w-lg bg-base-100 shadow-xl">
+            <div className="card-body">
+                <p>{review.review}</p>
+                <div className="flex items-center mt-5">
+                    <div className="avatar">
+                        <div className=" rounded-full ring ring-primary ring-offset-base-100 mr-5">
+                            <img src={review.img} alt="" />
+                        </div>
+                        <div>
+                            <h4 className='text-xl'>{review.name}</h4>
+                            <h4 className='text-xl'>{review.location}</h4>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default reviewCard;
--- src/Pages/Home/Reviews.js
+++ src/Pages/Home/Reviews.js
@@ -1,15 +1,43 @@
 import React from 'react';
+import Review from './Review'
 
 const Reviews = () => {
-    
+    const reviews = [
+        {
+            _id: 1,
+            name: 'Winson Herry',
+            review: 'It is a long established fact that by the readable content of a lot layout. The point of using Lorem a more-or-less normal distribu to using Content here, content',
+            location: 'california',
+            img: 'https://i.ibb.co/cYBsDbb/people1.png'
+        },
+        {
+            _id: 2,
+            name: 'Winson Herry',
+            review: 'It is a long established fact that by the readable content of a lot layout. The point of using Lorem a more-or-less normal distribu to using Content here, content',
+            location: 'california',
+            img: 'https://i.ibb.co/W0KqSrM/people2.png'
+        },
+        {
+            _id: 3,
+            name: 'Winson Herry',
+            review: 'It is a long established fact that by the readable content of a lot layout. The point of using Lorem a more-or-less normal distribu to using Content here, content',
+            location: 'california',
+            img: 'https://i.ibb.co/WPpcFgY/people3.png'
+        },
+    ];
 
 
 
     return (
-        <div>
-            <h3 className='text-xl text-primary font-bold py-2'>Latest Reviews</h3>
-            <div>
-
+        <div className='my-12 p-5'>
+            <h3 className='text-3xl text-primary font-bold py-2'>Latest Reviews</h3>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
+                {
+                    reviews.map(review => <Review
+                        key={review._id}
+                        review={review}
+                    ></Review>)
+                }
             </div>
         </div>
     );
