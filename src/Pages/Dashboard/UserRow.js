6fc465fe0bd4d1545679330b003685553e28ae19
proalamin-247 <pro.alamin247@gmail.com>
2022-05-26 14:35:27 +0600

    only an admin make a new admin and show error and succesfull message

--- src/Pages/Dashboard/UserRow.js
+++ src/Pages/Dashboard/UserRow.js
@@ -11,12 +11,19 @@ const UserRow = ({ user, refetch}) => {
                 authorization: `Bearer ${localStorage.getItem('accessToken')}`
             }
         })
-        .then(res=> res.json())
-        .then(data => {
-            // console.log(data)
-            refetch();
-            toast.success(`Successfully made an admin`);
-        })
+            .then(res => {
+                if (res.status === 403) {
+                    toast.error('Failed to make an admin')
+                }
+                return res.json()
+            })
+            .then(data => {
+                if (data.modifiedCount > 0) {
+                    refetch();
+                    toast.success(`Successfully made and admin`);
+                }
+                // refetch();
+            })
     }
 
     return (
